5ba43e75bb3e98a92fe0278585426dd6dccfe0ff
Ezabulbary <ezabul.bary@gmail.com>
2022-03-29 12:11:10 +0600

    inheritence

--- inheritence.js
+++ inheritence.js
@@ -1,20 +1,15 @@
 class Support {
     name;
     designation = 'support web dev';
-    address ='BD';
-    constructor(name, address){
+    address = 'BD';
+    constructor(name, address) {
         this.name = name;
         this.address = address;
     }
-    startSession(){
+    startSession() {
         console.log(this.name, 'start a support session');
     }
 }
 
 const amir = new Support('amir khan', 'India');
 const salman = new Support('salman khan', 'Dubai');
-amir.startSession();
-salman.startSession();
-
-/* console.log(amir);
-console.log(salman); */
