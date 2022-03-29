64d76c4b67e53872f53c269da365182cb1e6b592
Ezabulbary <ezabul.bary@gmail.com>
2022-03-29 19:54:30 +0600

    inheritence

--- inheritence.js
+++ inheritence.js
@@ -1,4 +1,4 @@
-class Support {
+class TeamMember {
     name;
     designation = 'support web dev';
     address = 'BD';
@@ -6,10 +6,40 @@ class Support {
         this.name = name;
         this.address = address;
     }
-    startSession() {
-        console.log(this.name, 'start a support session');
+}
+
+class Support extends TeamMember {
+    groupSupportTime;
+    constructor(name, address, time){
+        super(name, address)
+        this.groupSupportTime = time;
+    }
+    startSession(time) {
+        console.log(this.name, 'start a support session', time);
+    }
+}
+class StudentCare extends TeamMember {
+    
+    buildARoutin(student) {
+        console.log(this.name, 'Build a routin for', student);
+    }
+}
+class NeptuneDev extends TeamMember {
+    codeEditor;
+    constructor(name, address, editor){
+        super(name, address)
+        this.codeEditor = editor;
+    }
+    releaseApp(version) {
+        console.log(this.name, 'Release App version is', version);
     }
 }
 
 const amir = new Support('amir khan', 'India');
-const salman = new Support('salman khan', 'Dubai');
+amir.startSession(11);
+const salman = new Support('salman khan', 'Panjab', 4);
+console.log(salman);
+const srk = new StudentCare('SRK khan', 'Dilli');
+console.log(srk);
+const aks = new NeptuneDev('AKS kumar', 'Mumbai', 'nodepade++');
+aks.releaseApp('1.5.4');
